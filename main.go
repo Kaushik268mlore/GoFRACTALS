38db89c524f7b72d2b62f709ea0a0ea927e94287
kaushik kumar <kaushikkumarmlore@gmail.com>
2023-07-06 00:30:16 +0530

    created worker threads

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"log"
+	"math"
 	"time"
 
 	"github.com/faiface/pixel"
@@ -53,14 +55,66 @@ func run() {
 	img = image.NewRGBA(image.Rect(0, 0, imgWidth, imgHeight))
 	cfg := pixelgl.WindowConfig{
 		Title:  "Fractal in GOLANG",
-		Bounds: pixel.R(0, 0, imgHeight, imgWidth),
+		Bounds: pixel.R(0, 0, imgWidth, imgHeight),
 		VSync:  true,
 	}
-	win, err := pixelgl.WindowConfig(cfg)
+	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
 	log.Println("Generating..")
 	start := time.Now()
+	// start =Local().Sub()
+	workBuffer := make(chan workstruct, numblocks)
+	threadBuffer := make(chan bool, numThreads)
+	drawBuffer := make(chan pixstruct, pixelTotal)
+	workBufferInit(workBuffer)
+	go workersInit(drawBuffer, workBuffer, threadBuffer)
+	go drawThread(drawBuffer, win)
+	for !win.Closed() {
+		picture := pixel.PictureDataFromImage(img)
+		sprite := pixel.NewSprite(picture, picture.Bounds())
+		sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+		win.Update()
 
+	}
+	if showProgress {
+		fmt.Println("\r %d%d (%d%%)", pixelCount, pixelTotal, int(100*(float64(pixelCount)/float64(pixelTotal))))
+
+	}
+	if pixelCount == pixelTotal {
+		end := time.Now()
+		fmt.Println("Completed in ", end.Sub(start))
+		pixelCount++
+		if classOnEnd {
+			break
+		}
+	}
 }
+
+func workBufferInit(workBuffer chan workstruct) {
+	var sqt = int(math.Sqrt(numblocks))
+	for i := sqt - 1; i >= 0; i-- {
+		for j := 0; j < sqt; j++ {
+			workBuffer <- workstruct{
+				initX:  i * (imgWidth / sqt),
+				initY:  j * (imgHeight / sqt),
+				finalX: (i + 1) * (imgWidth / sqt),
+				finalY: (i + 1) * (imgWidth / sqt),
+			}
+		}
+	}
+}
+func workersInit(drawBuffer chan pixstruct, workBuffer chan workstruct, threadBuffer chan bool) {
+	for i := 1; i <= numThreads; i++ {
+		threadBuffer <- true
+	}
+	for range threadBuffer {
+		workstruct := <-workBuffer
+		go workerThread(workstruct, drawBuffer, threadBuffer)
+	}
+}
+func workerThread(workstruct workstruct, drawBuffer chan pixstruct, threadBuffer chan bool) {
+	// for x:=workstruct .initX;x<
+}
+func drawThread(drawBuffer chan pixstruct, win *pixelgl.Window)
